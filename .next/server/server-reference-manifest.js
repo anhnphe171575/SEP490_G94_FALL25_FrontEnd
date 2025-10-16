e55555a78bcd6b985cd4e9361627e4f710bba0bd
AnhNP <anhnphe171575@fpt.edu.vn>
2025-10-16 16:52:37 +0700

    Remove .next from tracking

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3vCWgfL+o9tY8QFlY2brLWiT5W6omp9o/nGXr+32A7o=\"\n}"
--- .next/static/chunks/src_app_layout_tsx_0a548d63._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__11aa3a12._.css",
-  "static/chunks/node_modules_63bd6274._.js",
-  "static/chunks/src_app_296257f1._.js"
-],
-    source: "dynamic"
-});
